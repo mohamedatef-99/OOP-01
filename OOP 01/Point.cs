9f6f7d679e4a39f66fe0384b3afb24484598d010
Mohamed Atef <Mohamed Atef>
2024-12-29 20:34:36 +0200

    Struct

---
+++ OOP 01/Point.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_01
+{
+    internal struct Point
+    {
+        //public int X;
+        //public int Y;
+
+
+        // counstructor : special method
+        // 1. same name as the struct
+        // 2. no return type
+
+        // CLR : will generate paremeterless constructor
+        // this constructor will init all fields with their default values
+
+
+
+        // Constructor
+        //public Point()
+        //{
+
+        //}
+
+        //public Point(int x, int y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
+
+
+        //public void Print()
+        //{
+        //    Console.WriteLine($"X = {X}, Y = {Y}");
+        //}
+
+        //public override string ToString()
+        //{
+        //    return $"({X}, {Y})";
+        //}
+    }
+}
--- OOP 01/Program.cs
+++ OOP 01/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace OOP_01
 {
+    // 1. Struct
+    // 2. Class
+    // 3. Interface
+    // 4. Enum
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Struct
+            // Struct : Value Types : Stack
+
+            //Point P01;
+            // Declare for object from type "Point"
+            // P01 : Object
+            // allocate uninitialized 8 bytes in the memory x=4 bytes, y=4 bytes
+
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P01.Y);
+
+            //P01.X = 10;
+            //P01.Y = 20;
+            //Console.WriteLine(P01.X);
+            //Console.WriteLine(P01.Y);
+
+            //Point P02 = new Point(2,3); // call the constructor
+            //Console.WriteLine(P02.X);
+            //Console.WriteLine(P02.Y);
+
+            //P01.Print(); 
+            #endregion
+
+
         }
     }
 }
